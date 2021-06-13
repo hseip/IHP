f700783c8e56c89590e5db2979f143937d516b9e
hseip <henning.seip@candogram.com>
2021-06-13 19:42:45 -0400

    test

--- AWS_install_scripts/installApp.sh
+++ AWS_install_scripts/installApp.sh
@@ -4,20 +4,26 @@
 #----------------------------------
 # install the node server
 #----------------------------------
-cd /home/ubuntu/BASE/server
+cp /home/ubuntu/env/envBASEServer /home/ubuntu/BASE/IHP/server/.env
+
+cd /home/ubuntu/BASE/IHP/server
 npm install
-
-# cp /home/ubuntu/server/.env /var/www/node/.env
-
-#start Node server for social media apis
-# systemctl start nodejs
 
 
 #----------------------------------
 # build the client app
 #----------------------------------
+cp /home/ubuntu/env/envBASEClient /home/ubuntu/BASE/IHP/client/.env
 
-cd /var/www/client
+cd /home/ubuntu/BASE/IHP/client
 npm install
 quasar build
+
+# copy bundle to the public directory
+cp /home/ubuntu/BASE/IHP/client/dist/spa/* /home/ubuntu/BASE/IHP/server/public
+
+
+#start Node server for social media apis
+systemctl start nodejs
+
 
