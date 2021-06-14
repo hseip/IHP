c0564b56a5094641aea30a5fb34cd723befaa474
hseip <henning.seip@candogram.com>
2021-06-13 20:51:15 -0400

    test: process.env

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 app.use('/api', apiRouter);
 
-
+console.log(`test NODE_ENV: ${process.env.NODE_ENV}`)
 if (process.env.NODE_ENV === 'PROD') {
 
     app.use(express.static(__dirname + '/public/'));
